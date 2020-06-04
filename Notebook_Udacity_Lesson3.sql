be00642228246eb4051113e7a5ca3b8c24ca7a54
nayidob <nayidob@gmail.com>
2020-06-04 18:15:51 -0500

    Update Notebook_Udacity_Lesson3.sql

--- Notebook_Udacity_Lesson3.sql
+++ Notebook_Udacity_Lesson3.sql
@@ -39,7 +39,7 @@ SELECT SUM(standard_qty) AS standard,
 	   SUM(gloss_qty) AS gloss,
 	   SUM(poster_qty) AS poster
 FROM orders;
-
+-- Alternative
 SELECT SUM(standard_amt_usd)/SUM(standard_qty) AS std_price_x_unit
 FROM orders;
 
@@ -248,7 +248,52 @@ GROUP BY 1
 ORDER BY 2 DESC
 LIMIT 1;
 
+## CASE Statement
+-- Equivalent of IF ELSE in SQL
+-- CASE goes in SELECT statement, which means, should be preceeded by a comma
+SELECT id, account_id, occurred_at, channel,
+       CASE WHEN channel = 'facebook' OR channel = 'direct' 
+       THEN 'yes' ELSE 'no' END AS is_facebook
+FROM web_events
+ORDER BY occurred_at;
 
+/*Creating manual bins*/
+SELECT account_id, occurred_at, total,
+       CASE WHEN total > 500 THEN 'Over 500'
+            WHEN total > 300 AND total <= 500 THEN '301 - 500'
+            WHEN total > 100 AND total <= 300 THEN '101 - 300'
+            ELSE '100 or under' END AS total_group
+FROM orders
 
+/*Example: Dealing with division by 0*/
+SELECT account_id, CASE WHEN standard_qty = 0 OR standard_qty IS NULL THEN 0
+                        ELSE standard_amt_usd/standard_qty END AS unit_price
+FROM orders
+LIMIT 10;
 
+/*Aggregation with CASE statement*/
+SELECT CASE WHEN total > 500 THEN 'Over 500'
+            ELSE '500 or under' END AS total_group,
+       COUNT(*) AS order_count
+FROM orders
+GROUP BY 1;
 
+/*Complete Example
+We would like to identify top performing sales reps, which are sales reps 
+associated with more than 200 orders or more than 750000 in total sales. 
+The middle group has any rep with more than 150 orders or 500000 in sales. 
+Create a table with the sales rep name, the total number of orders, total 
+sales across all orders, and a column with top, middle, or low depending on 
+this criteria. Place the top sales people based on dollar amount of sales 
+first in your final table.*/
+SELECT s.name, COUNT(*), SUM(o.total_amt_usd) total_spent, 
+     CASE WHEN COUNT(*) > 200 OR SUM(o.total_amt_usd) > 750000 THEN 'top'
+     WHEN COUNT(*) > 150 OR SUM(o.total_amt_usd) > 500000 THEN 'middle'
+     ELSE 'low' END AS sales_rep_level
+FROM orders o
+JOIN accounts a
+ON o.account_id = a.id 
+JOIN sales_reps s
+ON s.id = a.sales_rep_id
+GROUP BY s.name
+ORDER BY 3 DESC;
